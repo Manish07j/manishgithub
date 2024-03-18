08c73dbb7afe33b6d1c72577b0d9a310f5d06fd2
Manish07j <163849456+Manish07j@users.noreply.github.com>
2024-03-18 17:07:59 +0530

    Update Articlewriting.py

--- Articlewriting.py
+++ Articlewriting.py
@@ -1,29 +1,23 @@
 from openai import OpenAI
 import streamlit as st
-
-
-clt = OpenAI(
-    # defaults to os.environ.get("OPENAI_API_KEY")
-    api_key='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'
-)
-
+ 
+# Initialize the OpenAI client
+clt = OpenAI(api_key='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')
+ 
 def main():
     st.title("Article Writer")
     notes = st.text_area("Enter Topic Information:")
-    content = "I want you to write short literature review on topic "+notes
+    content = "I want you to write a short literature review on the topic: " + notes
     if st.button("Generate Article"):
         with st.spinner("Generating Article..."):
             response = clt.chat.completions.create(
-              model="gpt-3.5-turbo",
-              messages=[{'role':'user','content':content}]
+                model="text-davinci-003",
+                messages=[{'role': 'system', 'content': content}]
             )
         description = response.choices[0].message.content
         st.subheader("Generated Writeup:")
         st.write(description)
-
-
-
-
+ 
 if __name__ == '__main__':
-   main()
+    main()
 
